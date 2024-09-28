39d332555cf0431c414d9aae6f8b058635946d36
Duong.Do <vanduong.do2000@gmail.com>
2024-09-28 07:32:43 +0700

    update UI

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RegisterComponent } from "../register/register.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RegisterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit {
+  http = inject(HttpClient);
+  registerMode = false;
+  users: any;
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  registertoggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+  getUsers() {
+    this.http.get('http://localhost:5274/api/users').subscribe({
+        next: (response) => {
+            this.users = response;
+        },
+        error: (error) => {
+            console.error(error);
+        },
+        complete: () => {
+            console.log('Request completed');
+        },
+    });
+}
+} 
