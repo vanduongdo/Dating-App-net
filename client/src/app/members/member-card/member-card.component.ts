ae1a5135b9489f13a4e038e79806f6d84f546df3
@do.duong <vanduong.do2000@gmail.com>
2024-10-16 07:23:21 +0700

    update

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -6,7 +6,8 @@ import { Member } from '../../_models/member';
   standalone: true,
   imports: [],
   templateUrl: './member-card.component.html',
-  styleUrl: './member-card.component.css'
+  styleUrl: './member-card.component.css',
+  // encapsulation: ViewEncapsulation.Emulated,
 })
 export class MemberCardComponent {
   member = input.required<Member>();
