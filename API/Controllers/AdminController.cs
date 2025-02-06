6d477625536d9f302f5727021e7d8cc1d5bf81b8
@do.duong <vanduong.do2000@gmail.com>
2025-02-06 07:12:46 +0700

    update edit roles

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -25,6 +25,31 @@ public class AdminController(UserManager<AppUser> userManager) : BaseApiControll
         return Ok(users);
     }
 
+    [Authorize(Policy = "RequireAdminRole")]
+    [HttpPost("edit-roles/{username}")]
+    public async Task<ActionResult> EditRoles(string username, string roles)
+    {
+        if (string.IsNullOrEmpty(roles)) return BadRequest("You must select at least one role");
+
+        var selectedRoles = roles.Split(",").ToArray();
+
+        var user = await userManager.FindByNameAsync(username);
+
+        if (user == null) return BadRequest("Could not find user");
+
+        var userRoles = await userManager.GetRolesAsync(user);
+
+        var result = await userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+        if (!result.Succeeded) return BadRequest("Failed to add to roles");
+
+        result = await userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+        if (!result.Succeeded) return BadRequest("Failed to remove from roles");
+
+        return Ok(await userManager.GetRolesAsync(user));
+    }
+
     [Authorize(Policy = "ModeratorPhotoRole")]
     [HttpGet("photos-to-moderate")]
     public ActionResult GetPhotosForModeration()
