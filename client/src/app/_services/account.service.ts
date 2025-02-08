1f2d105522baa68a392c98f483a328b03cdac155
@do.duong <vanduong.do2000@gmail.com>
2025-02-08 10:14:13 +0700

    update role

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { User } from '../_models/user';
 import { map } from 'rxjs';
 import { environment } from '../../environments/environment';
@@ -16,6 +16,14 @@ export class AccountService {
   private likeService = inject(LikesService); // careful when injecting services into services to avoid circular dependencies
   baseUrl = environment.apiUrl;
   currentUser = signal<User | null>(null);
+  roles = computed<string[]>(() => {
+    const user = this.currentUser();
+    if (user && user.token) {
+      const role = JSON.parse(atob(user.token.split('.')[1])).role;
+      return Array.isArray(role) ? role : [role];
+    }
+    return [null];
+  });
 
   login(model: any) {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
